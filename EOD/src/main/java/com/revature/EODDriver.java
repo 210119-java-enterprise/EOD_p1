d78b3b02b76b6ad93d3580128c0fa3cbadee33c0
Cole Space <cospace@unm.edu>
2021-02-10 16:04:13 -0700

    added postgresql as a dependency onto the project

--- EOD/src/main/java/com/revature/EODDriver.java
+++ EOD/src/main/java/com/revature/EODDriver.java
@@ -1,11 +1,17 @@
 package com.revature;
 
+import com.revature.testmodels.User;
+import com.revature.util.EntityManager;
+
 /**
  * Simple class to test the library outside of the JUnit testing that is required
  */
 public class EODDriver {
 
     public static void main(String[] args) {
-
+        //TODO don't forget to change DB info
+        EntityManager manager = new EntityManager("src/main/resources/simpleUserTest.properties");
+        manager.addAnnotatedClass(User.class);
+        System.out.println("Welcome");
     }
 }
