f61b7b2c09271fee9b4124f095b3b152115f80c0
Cole Space <cospace@unm.edu>
2021-02-09 17:33:12 -0700

    Created the services package which holds the services for each of the util java files. These services will handle checks to make sure the information passed by the user is correct and there are no syntax errors or use errors.

---
+++ EOD/src/main/java/com/revature/services/DataManipulationService.java
@@ -0,0 +1,9 @@
+package com.revature.services;
+
+/**
+ * This class acts as an intermediary for the DataManipulation, which will take in
+ * user input. This class will make sure that the user input is following the
+ * correct implementation of this library before sending the information to the DAO.
+ */
+public class DataManipulationService {
+}
