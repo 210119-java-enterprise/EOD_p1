72dfd677b04ed48b7c8aea71ee5864667763504d
Cole Space <cospace@unm.edu>
2021-02-09 17:31:57 -0700

    Created the DAO package, which holds all of the dao files. Each dao file represents the type of sql statements that are executed by the dao type. DataManipualtionDAO is for DML queries, DataQueryDAO is for DQL queries, and TransactionControlDAO is for TCL statements

---
+++ EOD/src/main/java/com/revature/dao/TransactionControlDAO.java
@@ -0,0 +1,9 @@
+package com.revature.dao;
+
+/**
+ * This class speaks to the database with a user specified TCL statement
+ * and will return with whether or not the DML statement was successfully
+ * executed or not.
+ */
+public class TransactionControlDAO {
+}
