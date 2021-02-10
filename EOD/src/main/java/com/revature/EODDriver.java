7cd822dbed39cae407c7d7a9e267f808d4baf512
Cole Space <cospace@unm.edu>
2021-02-10 14:54:57 -0700

    Created a driver class to run and test the library without needding to write any JUnit tests

---
+++ EOD/src/main/java/com/revature/EODDriver.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+/**
+ * Simple class to test the library outside of the JUnit testing that is required
+ */
+public class EODDriver {
+
+    public static void main(String[] args) {
+
+    }
+}
