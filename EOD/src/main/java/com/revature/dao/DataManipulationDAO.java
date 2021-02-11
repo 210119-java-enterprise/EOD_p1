2b1b92e308c40b9e5e2982ff0e09b85f2fc3ceab
Cole Space <cospace@unm.edu>
2021-02-10 17:23:46 -0700

    started to implement the insert method for the DML dao

--- EOD/src/main/java/com/revature/dao/DataManipulationDAO.java
+++ EOD/src/main/java/com/revature/dao/DataManipulationDAO.java
@@ -1,9 +1,21 @@
 package com.revature.dao;
 
+import com.revature.models.Insert;
+import com.revature.util.Metamodel;
+
 /**
  * This class speaks to the database with a user specified DML statement
  * and will return with whether or not the DML statement was successfully
  * executed or not.
  */
 public class DataManipulationDAO {
+
+    public Object checkIfObjectIsInUse(Metamodel<?> model, Object object){
+        return null;
+    }
+
+    public void insert(Metamodel<?> model, Object object){
+        //TODO for prepared statements, you need to assign the values to ? in the statement string
+        Insert insertStatement = new Insert(model, object);
+    }
 }
