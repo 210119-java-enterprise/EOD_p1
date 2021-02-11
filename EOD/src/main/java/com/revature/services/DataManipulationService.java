67d99e6a72a94aa0b28ed3bf0a4519145cc99fc5
Cole Space <cospace@unm.edu>
2021-02-10 17:26:58 -0700

    started to implement the basic logic checks for the service class, as well as how the class talks to the dao

--- EOD/src/main/java/com/revature/services/DataManipulationService.java
+++ EOD/src/main/java/com/revature/services/DataManipulationService.java
@@ -1,9 +1,33 @@
 package com.revature.services;
 
+import com.revature.annotations.Table;
+import com.revature.dao.DataManipulationDAO;
+import com.revature.util.Metamodel;
+
 /**
  * This class acts as an intermediary for the DataManipulation, which will take in
  * user input. This class will make sure that the user input is following the
  * correct implementation of this library before sending the information to the DAO.
  */
 public class DataManipulationService {
+
+    private DataManipulationDAO dmlDao;
+
+    public DataManipulationService(DataManipulationDAO dao){
+        dmlDao = dao;
+    }
+
+    public void insert(Metamodel<?> model, Object object){
+        if(!isValidObject(object)){
+            throw new RuntimeException("Invalid user, user is null");
+        }
+        if(dmlDao.checkIfObjectIsInUse(model, object) == null){
+            throw new RuntimeException("Object already in database");
+        }
+        dmlDao.insert(model, object);
+    }
+
+    private boolean isValidObject(Object object){
+        return (object != null);
+    }
 }
