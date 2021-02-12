f5a23f62626f645de4b4728e746a5737c674a21a
Cole Space <cospace@unm.edu>
2021-02-12 09:06:32 -0700

    created a new config, which gave a new entity manager, which can hand out new sessions

--- EOD/src/main/java/com/revature/EODDriver.java
+++ EOD/src/main/java/com/revature/EODDriver.java
@@ -1,7 +1,9 @@
 package com.revature;
 
 import com.revature.testmodels.User;
+import com.revature.util.Configuration;
 import com.revature.util.EntityManager;
+import com.revature.util.Session;
 
 /**
  * Simple class to test the library outside of the JUnit testing that is required
@@ -9,21 +11,26 @@ import com.revature.util.EntityManager;
 public class EODDriver {
 
     public static void main(String[] args) {
-        EntityManager manager = new EntityManager("src/main/resources/simpleUserTest.properties");
-        manager.addAnnotatedClass(User.class);
+
+        Configuration config = new Configuration("src/main/resources/simpleUserTest.properties");
+        config.addAnnotatedClass(User.class);
+
+        EntityManager manager = config.createEntityManager();
+        Session session = manager.getSession();
 
         User user1 = new User("Cole", "Space", "username", "password");
         User user2 = new User("John", "John", "test", "12345");
         User user3 = new User("Cole","Space","diff","09876");
-        manager.save(user1);
+
+        session.save(user1);
         System.out.println("saved user 1");
-        manager.save(user2);
+        session.save(user2);
         System.out.println("saved user2");
-        manager.update(user3, user1);
+        session.update(user3, user1);
         System.out.println("Updated user1 with user3");
-        manager.delete(user2);
+        session.delete(user2);
         System.out.println("Deleted user2");
-        manager.delete(user3);
+        session.delete(user3);
         System.out.println("Deleted user3 that was user1");
     }
 }
