70c8e80d3ba52afec31612d61141c8353a9d7eb9
Cole Space <cospace@unm.edu>
2021-02-11 17:44:45 -0700

    implemented the delete and update sql statemets within the dao and verified that they work

--- EOD/src/main/java/com/revature/dao/DataManipulationDAO.java
+++ EOD/src/main/java/com/revature/dao/DataManipulationDAO.java
@@ -1,7 +1,9 @@
 package com.revature.dao;
 
 import com.revature.annotations.Column;
+import com.revature.models.Delete;
 import com.revature.models.Insert;
+import com.revature.models.Update;
 import com.revature.util.Metamodel;
 import static com.revature.util.EntityManager.getConnectionFactory;
 
@@ -23,9 +25,89 @@ public class DataManipulationDAO {
         return null;
     }
 
+    /**
+     * Inserts some user specified data into a database
+     * @param model the model of the class being inserted
+     * @param object the data being persisted
+     */
     public void insert(Metamodel<?> model, Object object){
         //TODO for prepared statements, you need to assign the values to ? in the statement string
         Insert insertStatement = new Insert(model, object);
+        ArrayList<String> objectValues = getObjectValues(object);
+
+        //System.out.println(insertStatement.getInsertStatement());
+        try(Connection conn = getConnectionFactory().getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(insertStatement.getInsertStatement());
+
+            for(int i = 0; i < objectValues.size(); i++){
+                pstmt.setObject(i + 1, objectValues.get(i));
+            }
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates some data into a database
+     * @param model the model of the class being inserted
+     * @param newObject the data being updated
+     * @param oldObject the data being overwritten
+     */
+    public void update(Metamodel<?> model, Object newObject, Object oldObject){
+        Update updateStatement = new Update(model, oldObject);
+        ArrayList<String> oldObjectValues = getObjectValues(oldObject);
+        ArrayList<String> newObjectValues = getObjectValues(newObject);
+        int bound = oldObjectValues.size();
+
+        System.out.println(updateStatement.getUpdateStatement());
+        try(Connection conn = getConnectionFactory().getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(updateStatement.getUpdateStatement());
+
+            for(int i = 0; i < bound; i++){
+                pstmt.setObject(i+1, newObjectValues.get(i));
+                pstmt.setObject(i+5, oldObjectValues.get(i));
+            }
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Deletes some user specified data into a database
+     * @param model the model of the class being deleted
+     * @param object the data being deleted
+     */
+    public void delete(Metamodel<?> model, Object object){
+        Delete deleteStatement = new Delete(model, object);
+        ArrayList<String> objectValues = getObjectValues(object);
+
+        try(Connection conn = getConnectionFactory().getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(deleteStatement.getDeleteStatement());
+
+            for(int i = 0; i < objectValues.size(); i++){
+                pstmt.setObject(i + 1, objectValues.get(i));
+            }
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Scrapes the object passed by the user and grabs all of the values within
+     * the object
+     * @param object the object being scrapped
+     * @return an array list of all of the values within the object
+     */
+    private ArrayList<String> getObjectValues(Object object){
         ArrayList<String> objectValues = new ArrayList<>();
         Field[] fields = object.getClass().getDeclaredFields();
         for(Field f : fields){
@@ -40,19 +122,6 @@ public class DataManipulationDAO {
                 }
             }
         }
-        System.out.println(insertStatement.getInsertStatement());
-        //open a connection
-        try(Connection conn = getConnectionFactory().getConnection()){
-            PreparedStatement pstmt = conn.prepareStatement(insertStatement.getInsertStatement());
-
-            for(int i = 0; i < objectValues.size(); i++){
-                pstmt.setObject(i + 1, objectValues.get(i));
-            }
-
-            pstmt.executeUpdate();
-
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
+        return objectValues;
     }
 }
