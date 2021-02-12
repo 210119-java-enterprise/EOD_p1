65d7f73334bbb1e78816a2f2786f11772b6e5028
Cole Space <cospace@unm.edu>
2021-02-11 17:45:59 -0700

    implemented the update and delete statementsfor the DMS

--- EOD/src/main/java/com/revature/services/DataManipulationService.java
+++ EOD/src/main/java/com/revature/services/DataManipulationService.java
@@ -17,6 +17,11 @@ public class DataManipulationService {
         dmlDao = dao;
     }
 
+    /**
+     * Basic implementation of the insert sql statement
+     * @param model the metamodel of the class type
+     * @param object the instantiation of the class
+     */
     public void insert(Metamodel<?> model, Object object){
         if(!isValidObject(object)){
             throw new RuntimeException("Invalid user, user is null");
@@ -27,6 +32,42 @@ public class DataManipulationService {
         dmlDao.insert(model, object);
     }
 
+    /**
+     * Basic implementation of the delete sql statement
+     * @param model the metamodel of the class type
+     * @param object the instantiation of the class
+     */
+    public void delete(Metamodel<?> model, Object object){
+        if(!isValidObject(object)){
+            throw new RuntimeException("Invalid user, user is null");
+        }
+//        if(dmlDao.checkIfObjectIsInUse(model, object) == null){
+//            throw new RuntimeException("Object is not in database");
+//        }
+        dmlDao.delete(model, object);
+    }
+
+    /**
+     * Basic implementation of the update sql statement
+     * @param model the metamodel of the class type
+     * @param newObject the instantiation of the new object that will update the old entry
+     * @param oldObject the instantiation of the old object already in the DB
+     */
+    public void update(Metamodel<?> model, Object newObject, Object oldObject){
+        if(!isValidObject(newObject) | !isValidObject(oldObject)){
+            throw new RuntimeException("Invalid object passed");
+        }
+//        if(dmlDao.checkIfObjectIsInUse(model, oldObject) == null){
+//            throw new RuntimeException("Object is not in database, try inserting before updating");
+//        }
+        dmlDao.update(model, newObject, oldObject);
+    }
+
+    /**
+     * Checks to see if the object passed is non null
+     * @param object the object being persisted to the DB
+     * @return true if valid, false if not
+     */
     private boolean isValidObject(Object object){
         return (object != null);
     }
