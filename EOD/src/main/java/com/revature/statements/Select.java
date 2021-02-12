0015fb6a77cabd538baf920d3bfa2dca4d59e70b
Cole Space <cospace@unm.edu>
2021-02-12 11:00:02 -0700

    deleted unused lines within the insert class

--- EOD/src/main/java/com/revature/statements/Select.java
+++ EOD/src/main/java/com/revature/statements/Select.java
@@ -1,13 +1,10 @@
 package com.revature.statements;
 
 import com.revature.annotations.Table;
-import com.revature.util.ColumnField;
 import com.revature.util.Metamodel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Select {
 
