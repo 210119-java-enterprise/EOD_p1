afffbb1bfaa88f82bb22776e58d29179906e86a7
Cole Space <cospace@unm.edu>
2021-02-11 18:12:48 -0700

    added more users and changes to users to try and replicate error, was not able to. I guess is fixed for now

--- EOD/src/main/java/com/revature/EODDriver.java
+++ EOD/src/main/java/com/revature/EODDriver.java
@@ -12,16 +12,18 @@ public class EODDriver {
         EntityManager manager = new EntityManager("src/main/resources/simpleUserTest.properties");
         manager.addAnnotatedClass(User.class);
 
-        User user = new User("Cole", "Space", "username", "password");
-        User updatedUser = new User("Cole", "Space", "test", "12345");
-//        manager.save(user);
-//        manager.save(updatedUser);
-//        System.out.println("Saved user");
-//        manager.update(updatedUser, user);
-//        System.out.println("Updated user");
-//        manager.delete(updatedUser);
-//        System.out.println("Deleted updated user");
-//        manager.delete(user);
-//        System.out.println("Deleted old user if update didn't work");
+        User user1 = new User("Cole", "Space", "username", "password");
+        User user2 = new User("John", "John", "test", "12345");
+        User user3 = new User("Cole","Space","diff","09876");
+        manager.save(user1);
+        System.out.println("saved user 1");
+        manager.save(user2);
+        System.out.println("saved user2");
+        manager.update(user3, user1);
+        System.out.println("Updated user1 with user3");
+        manager.delete(user2);
+        System.out.println("Deleted user2");
+        manager.delete(user3);
+        System.out.println("Deleted user3 that was user1");
     }
 }
