f9bdbd9dcdd1e0cb397708abbd178019499885ed
Cole Space <cospace@unm.edu>
2021-02-12 19:38:42 -0700

    tested to make sure serial and non serial pk will work

--- EOD/src/main/java/com/revature/EODDriver.java
+++ EOD/src/main/java/com/revature/EODDriver.java
@@ -28,22 +28,22 @@ public class EODDriver {
         User user2 = new User("John", "John", "test", "12345");
         User user3 = new User("Cole","Space","diff","09876");
 
-//        session1.save(user1);
-//        System.out.println("saved user 1");
-//        session1.save(user2);
-//        System.out.println("saved user2");
-//        List<User> firstQuery = (List<User>) session1.selectAll(user1);
-//        firstQuery.forEach(System.out::print);
-//        System.out.println();
-//        List<User> secondQuery = (List<User>) session1.selectFrom(user1, "user_id", "username", "password");
-//        secondQuery.forEach(System.out::print);
-//        System.out.println();
-//        session1.update(user3, user1);
-//        System.out.println("Updated user1 with user3");
-//        session1.delete(user2);
-//        System.out.println("Deleted user2");
-//        session1.delete(user3);
-//        System.out.println("Deleted user3 that was user1");
+        session1.save(user1);
+        System.out.println("saved user 1");
+        session1.save(user2);
+        System.out.println("saved user2");
+        List<User> firstQuery = (List<User>) session1.selectAll(user1);
+        firstQuery.forEach(System.out::print);
+        System.out.println();
+        List<User> secondQuery = (List<User>) session1.selectFrom(user1, "user_id", "username", "password");
+        secondQuery.forEach(System.out::print);
+        System.out.println();
+        session1.update(user3, user1);
+        System.out.println("Updated user1 with user3");
+        session1.delete(user2);
+        System.out.println("Deleted user2");
+        session1.delete(user3);
+        System.out.println("Deleted user3 that was user1");
 
         Band trivium = new Band("Trivium", "Heafy", "Corey", "Heafy", "Paulo");
         session2.save(trivium);
